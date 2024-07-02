3fc47225ce12376eb4834d06e67851dbbde57663
Lebohang Motseki <lbmotseki@gmail.com>
2024-07-02 09:50:17 +0000

    Set data Structure Correection

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,9 @@
-export default function setFromArray(arr) {
-  return new Set(arr);
+/**
+ * Creates a new Set from the elements in the given array.
+ *
+ * @param {Array} array - The array to create the Set from
+ * @return {Set} A new Set containing the elements from the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
 }
